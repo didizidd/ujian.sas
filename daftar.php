ed1904dcc2f673cf2d421323d636a77293e4b306
didizidd <hafizudi192gmail.com>
2026-05-26 09:12:33 +0700

    first commit

---
+++ daftar.php
@@ -0,0 +1,25 @@
+<?php
+include "koneksi.php";
+if(isset ($_POST["submid"])){
+
+$username=$_POST["username"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+
+$query = "INSERT INTO tb_users (username,email,password) VALUES ('username','email','password')";
+$eksekusi = mysqli_query ($koneksi,$query);
+
+}
+?>
+<center>
+<form action="daftar.php" method="POST">
+    <label for="">username</label>
+    <input type="text" name="username" placeholder="username"><br><br>
+    <label for="">email</label>
+    <input type="email" name="email" placeholder="email"><br><br>
+    <label for="">password</label>
+    <input type="password" name="password" placeholder="password"><br><br>
+
+    <button type="submid" name="submid">daftar</button>
+</form>
+</center>
